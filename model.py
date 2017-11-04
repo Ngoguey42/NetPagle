cb0a1eeb5eb3c73fc166640103b7b98cbdfc3beb
Ngoguey42 <ngoguey@airware.com>
2017-11-04 22:21:51 +0100

    greatly improve graph

--- model.py
+++ model.py
@@ -8,7 +8,7 @@ import time
 import datetime
 import scipy.ndimage as ndi
 import matplotlib.pyplot as plt
-
+import pandas as pd
 
 import accuracy
 from constants import *
@@ -29,42 +29,34 @@ class Model(object):
 
         self.ds = ds
         self.directory = directory
-        self.epoch_count_path = os.path.join(directory, 'epoch_count')
         self.model_names = self.create_model_names()
         self.previous_save = None
 
         if len(self.model_names) == 0:
             print('Creating keras model from scratch...')
             self.m = keras_model.create_model()
-            self.epoch_count = 0
-            open(self.epoch_count_path, 'w')
-            assert self.read_epoch_count() == 0
         else:
             path = os.path.join(self.directory, self.model_names[-1] + '.hdf5')
             print('Creating keras model from file {}'.format(
                 path
             ))
             self.m = keras.models.load_model(path)
-            self.epoch_count = self.read_epoch_count()
-            print('  {} epochs'.format(self.epoch_count))
 
-        self.lrs = np.full([self.epoch_count], 0, dtype='float64')
-        self.losses = np.full([self.epoch_count], 0, dtype='float64')
-        self.acctrains = np.full([self.epoch_count], 0, dtype='float64')
-        self.acctests = np.full([self.epoch_count], 0, dtype='float64')
 
+        self.df = pd.DataFrame()
         for model_name in self.model_names:
             model_info = model_name.split('_')
-            time, epoch, lr, loss, acc, trainacc, testacc, lastname = model_info
-            epoch = int(epoch)
-            lr = float(lr)
-            self.lrs[epoch] = float(lr)
-            self.losses[epoch] = float(loss)
-            self.acctrains[epoch] = float(trainacc)
-            self.acctests[epoch] = float(testacc)
+            time, epoch, lr, loss, acc, acctrain, acctest, lastname = model_info
 
-    def read_epoch_count(self):
-        return len(open(self.epoch_count_path, 'r').read())
+            self.df = self.df.append(pd.Series({
+                'epoch': int(epoch),
+                'lr': float(lr),
+                'loss': float(loss),
+                'acctrain': float(acctrain),
+                'acctest': float(acctest),
+            }), ignore_index=True)
+        if self.df.size > 0:
+            self.df.loc[:, 'epoch'] = pd.to_numeric(self.df.epoch, downcast='integer')
 
     def create_model_names(self):
         return sorted([fname[:-5]
@@ -74,13 +66,14 @@ class Model(object):
 
     def on_epoch_end(self, epoch, logs=None):
         print()
-        open(self.epoch_count_path, 'a').write('x')
-        self.epoch_count += 1
+        if self.df.size == 0:
+            epoch = 0
+        else:
+            epoch = int(self.df.epoch.max() + 1)
 
         loss = logs.get('loss', 42)
         acc = logs.get('acc', 42)
         lr = keras.backend.eval(self.m.optimizer.lr)
-        epoch = self.epoch_count - 1
 
         t = datetime.datetime.fromtimestamp(time.mktime(time.localtime()))
         t = time.strftime(time_format)
@@ -88,6 +81,15 @@ class Model(object):
         info = self.eval_accuracies()
         acctrain = info['acctrain']
         acctest = info['acctest']
+
+        self.df = self.df.append(pd.Series({
+            'epoch': epoch,
+            'lr': lr,
+            'loss': loss,
+            'acctrain': acctrain,
+            'acctest': acctest,
+        }), ignore_index=True)
+
 
         fmt = '_'.join([
             '{}', # time
@@ -100,12 +102,6 @@ class Model(object):
             '{}', # lastname
         ])
         model_name = fmt.format(t, epoch, lr, loss, acc, acctrain, acctest, lastname)
-
-        self.lrs = np.asarray(self.lrs.tolist() + [lr])
-        self.losses = np.asarray(self.losses.tolist() + [loss])
-        self.acctrains = np.asarray(self.acctrains.tolist() + [acctrain])
-        self.acctests = np.asarray(self.acctests.tolist() + [acctest])
-
         # now = datetime.datetime.now()
         # if self.previous_save is None or (now - self.previous_save).total_seconds() > self.delta_save:
             # self.previous_save = now
@@ -113,8 +109,6 @@ class Model(object):
         self.m.save(os.path.join(
             self.directory, model_name + '.hdf5'
         ))
-        print('-------------------------------------------------- Asking for refresh')
-        # self.refresh = True
         self.show_board()
 
     def eval_accuracies(self):
@@ -152,39 +146,80 @@ class Model(object):
 
     def show_board(self):
         def _show_accuracy(ys, color, label):
-            label = '{} (max={})'.format(label, ys.max())
-            ax1.plot(ndi.gaussian_filter1d(ys, 3), lw=1, alpha=1, ls='-', c=color, label=label, zorder=3)
-            above = np.maximum.accumulate(ys)
-            below = np.minimum.accumulate(ys[::-1])[::-1]
-            above = ndi.gaussian_filter1d(above, 2)
-            below = ndi.gaussian_filter1d(below, 2)
+            ax1.plot(
+                self.df.epoch, _smooth(ys, 2),
+                lw=1, alpha=1, ls='-', c=color, zorder=3,
+                label='{} (max={})'.format(label, ys.max()),
+            )
             ax1.fill_between(
-                range(above.size), above, below,
+                self.df.epoch,
+                # np.maximum.accumulate(ys),
+                # np.minimum.accumulate(ys[::-1])[::-1],
+                _smooth(np.maximum.accumulate(ys), 1),
+                _smooth(np.minimum.accumulate(ys[::-1])[::-1], 1),
                 facecolor=color,
                 alpha=1/3,
                 zorder=0,
             )
 
+        def _steps():
+            i = 0
+            start = 0
+            step = 3
+            while start <= border_dist.max():
+                yield i, start, start + step
+                start += step
+                step += 1
+                i += 1
+
+        def _kernel(radius, strength):
+            a = np.zeros(radius * 2 + 1, float)
+            a[radius] = 1
+            a = ndi.gaussian_filter1d(a, radius / (4 / strength))
+            return a
+
+        def _smooth(y, strength):
+            res = np.zeros(y.shape, dtype=float)
+            for radius, start, end in _steps():
+                kernel = _kernel(radius, strength)
+                res += (
+                    ndi.filters.convolve1d(y, kernel, mode='nearest') *
+                    ((border_dist >= start) & (border_dist < end))
+                )
+            return res
+
+        if self.df.shape[-1] < 2:
+            print('No plot when less than 2 elements!')
+            return
+
+        border_dist = np.min(
+            np.c_[self.df.epoch, self.df.epoch.max() - self.df.epoch],
+            axis=1,
+        )
+
         fig, ax1 = plt.subplots(figsize=(16, 9))
-        _show_accuracy(self.acctrains, 'red', 'Accuracy train set')
-        _show_accuracy(self.acctests, 'green', 'Accuracy test set')
-        ax1.plot(self.lrs, lw=2, alpha=1/3, ls='-', c='orange', label='Learning rate', zorder=1)
+        _show_accuracy(self.df.acctrain, 'red', 'Accuracy train set')
+        _show_accuracy(self.df.acctest, 'green', 'Accuracy test set')
+        ax1.plot(
+            self.df.epoch, self.df.lr,
+            lw=2, alpha=1/3, ls='-', c='orange', label='Learning rate', zorder=1
+        )
         ax1.set_xlabel('epoch')
         ax1.set_ylabel('percent')
-        plt.legend(loc='center right')
+        plt.legend(loc='lower center')
 
         ax2 = ax1.twinx()
-        mask = self.losses < self.losses.min() + self.losses.std() * 3
+        mask = self.df.loss < self.df.loss.min() + self.df.loss.std() * 3
         ax2.plot(
-            mask.nonzero()[0],
-            self.losses[mask],
+            self.df.epoch[mask],
+            self.df.loss[mask],
             lw=1, alpha=1, ls='-', c='purple',
-            label='loss (min={:.8f})'.format(self.losses.min()),
+            label='loss (min={:.8f})'.format(self.df.loss.min()),
             zorder=2,
         )
         ax2.set_ylabel('loss', color='purple')
         ax2.tick_params('y', colors='purple')
-        plt.legend(loc='lower center')
+        plt.legend(loc='lower left')
 
         plt.tight_layout()
 
